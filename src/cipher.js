91c0392c8767e3b080c4c280426427bc6191b3dd
BrigitteNikoll <BrigitteNikoll@users.noreply.github.com>
2021-01-26 11:58:22 -0500

    Añadiendo lectura de otro caracteres

--- src/cipher.js
+++ src/cipher.js
@@ -18,9 +18,10 @@ const cipher = {
               nuevoTexto += String.fromCharCode(((str.charCodeAt(i) - 65 + parseInt(offset)) % 26) + 65);
           } else if (str.charCodeAt(i) >= 97 &&  str.charCodeAt(i) <= 122) {
               nuevoTexto += String.fromCharCode(((str.charCodeAt(i) - 97 + parseInt(offset)) % 26) + 97);
-          } else if (str.charCodeAt(i === 32)) {
-            nuevoTexto += " ";
+          } else {
+            nuevoTexto += str[i];
           }
+            
       }
       
       return nuevoTexto; 
@@ -44,8 +45,8 @@ const cipher = {
               nuevoTexto += String.fromCharCode(((str.charCodeAt(i) - 90 - parseInt(offset)) % 26) + 90);
           } else if (str.charCodeAt(i) >= 97 &&  str.charCodeAt(i) <= 122) {
               nuevoTexto += String.fromCharCode(((str.charCodeAt(i) - 122 - parseInt(offset)) % 26) + 122);
-          } else if (str.charCodeAt(i === 32)) {
-            nuevoTexto += " ";
+          } else {
+            nuevoTexto += str[i];
           }
       }
       
